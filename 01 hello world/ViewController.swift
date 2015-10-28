ffafad6992eb842251ac4bca4cb97eb1358fe437
Bogdan Badea <bogdan.badea@stormbraces.com>
2015-10-28 13:22:53 +0200

    primul meu commit

--- 01 hello world/ViewController.swift
+++ 01 hello world/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hitMe_BTN: UIButton!
+    @IBOutlet weak var helloWorld_IMG: UIImageView!
+    @IBOutlet weak var again_BTN: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloWorld_IMG.hidden = true;
+        hitMe_BTN.hidden = false;
+        again_BTN.hidden = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hitMe_BTN(sender: UIButton) {
+        helloWorld_IMG.hidden = false;
+        sender.hidden = true;
+        again_BTN.hidden = false;
+    }
 
+    @IBAction func again_BTN(sender: UIButton) {
+        sender.hidden = true;
+        hitMe_BTN.hidden = false;
+        helloWorld_IMG.hidden = true;
+
+    }
 }
 
