2fe2d83be5114f7fb1c018212afd46d76bfdf7bc
Bogdan Badea <bogdan.badea@stormbraces.com>
2015-10-28 10:45:01 +0200

    Initial Commit

---
+++ 01 hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01 hello world
+//
+//  Created by Bibi Badea on 10/28/15.
+//  Copyright © 2015 Stormbraces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
